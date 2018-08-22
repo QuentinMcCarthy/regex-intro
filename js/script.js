bb6c11e1e6be4ce2d2cc78f85a4d1ca82d0ecb2f
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-23 11:29:28 +1200

    Login form validation

--- js/script.js
+++ js/script.js
@@ -24,3 +24,95 @@ function validatePhPost(){
 		console.log("Postal Code correct");
 	}
 }
+
+function validateLogin(){
+	event.preventDefault();
+
+	var username = document.getElementById("username").value,
+		nameRegex = /^[\w]{6,12}$/;
+
+	if(!username){
+		alert("Enter: Username cannot be blank!");
+	} else{
+		let count = /^.{6,15}$/g,
+			cap = /(.*[A-Z]){1,}/;
+			lwr = /.*[a-z]/;
+			num = /(.*\d){1,}/;
+
+		let countResult = count.test(username);
+
+		if(!countResult){
+			console.error("Invalid username length");
+			alert("Username must be between 6-15 chars long");
+		} else{
+			let capResult = cap.test(username);
+
+			if(!capResult){
+				console.error("Invalid username capitalisation");
+				alert("Username must contain at least one capital!");
+			} else{
+				let lwrResult = lwr.test(username);
+
+				if(!lwrResult){
+					console.error("Invalid username capitalisation");
+					alert("Username must contain at least one lowercase letter!");
+				} else{
+					let numResult = num.test(username);
+
+					if(!numResult){
+						console.error("Invalid username numerics");
+						alert("Username must contain at least one number!");
+					} else{
+						console.log("Username valid");
+					}
+				}
+			}
+		}
+	}
+
+	var password = document.getElementById("password").value,
+		confirmPass = document.getElementById("confirmPass").value,
+		passRegex = /^[\w]{6,12}$/;
+
+	if(!password){
+		alert("Error: Password cannot be blank!");
+	} else if(password !== confirmPass){
+		console.error("Password mismatch");
+		alert("Error: Passwords do not match!");
+	} else{
+		let count = /^.{9,}$/g,
+			cap = /(.*[A-Z]){2,}/;
+			lwr = /.*[a-z]/;
+			num = /(.*\d){2,}/;
+
+		let countResult = count.test(password);
+
+		if(!countResult){
+			console.error("Invalid password length");
+			alert("Password must be at least 9 chars long");
+		} else{
+			let capResult = cap.test(password);
+
+			if(!capResult){
+				console.error("Invalid password capitalisation");
+				alert("Password must contain at least two capitals!");
+			} else{
+				let lwrResult = lwr.test(password);
+
+				if(!lwrResult){
+					console.error("Invalid password capitalisation");
+					alert("Password must contain at least one lowercase letter!");
+				} else{
+					let numResult = num.test(password);
+
+					if(!numResult){
+						console.error("Invalid password numerics");
+						alert("Password must contain at least two numbers!");
+					} else{
+						console.log("Password valid");
+					}
+				}
+			}
+		}
+	}
+}
